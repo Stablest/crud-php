afce6aed90f616d121512565a0bf77e283379e04
Stablest <marlonlocci@hotmail.com>
2023-08-08 14:42:16 -0300

    bug-fix: Added $errors array

--- includes/register/register-user-controller.inc.php
+++ includes/register/register-user-controller.inc.php
@@ -3,16 +3,17 @@
 declare(strict_types=1);
 
 function check_valid_input($pdo, $username, $pwd, $email) {
+    $errors = [];
     if (empty($username) || empty($pwd) || empty($email))
-        $errors["invalid_input"] = 'Invalid input';
-    if (filter_var($email, FILTER_VALIDATE_EMAIL))
-        $errors["invalid_email"] = 'Invalid email';
+        $errors["invalid_input"] = 'Por favor preencha todos os campos';
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL))
+        $errors["invalid_email"] = 'E-mail inválido';
     $compared_username = get_username($pdo, $username);
     if ($compared_username)
-        $errors["username already exists"] = 'Username already exists';
+        $errors["username already exists"] = 'Usuário já existe';
     $compared_email = get_email($pdo, $email);
     if ($compared_email)
-        $errors["email already registered"] = 'Email already registered';
+        $errors["email already registered"] = 'E-mail já registrado';
     return $errors;
 }
 
