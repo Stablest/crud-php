0c62e900fd6a93fa8e31541fcb4c6624c9136aaf
Stablest <marlonlocci@hotmail.com>
2023-08-08 08:32:21 -0300

    feat: Added check_signup_errors function

---
+++ includes/register/register-user-view.inc.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+function check_signup_errors() {
+    if (isset($_SESSION['errors_register'])) {
+        $errors = $_SESSION['errors_register'];
+
+        foreach ($errors as $error) {
+            echo '<p>' . $error . '</p>';
+        }
+
+        unset($_SESSION['errors_register']);
+    }
+}
