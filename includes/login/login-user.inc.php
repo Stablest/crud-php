5ba00f6039793d5a27eafceb50f7190f4970e3ac
Stablest <marlonlocci@hotmail.com>
2023-08-08 14:41:31 -0300

    feat: Added checks for errors

---
+++ includes/login/login-user.inc.php
@@ -0,0 +1,40 @@
+<?php
+
+if($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $email = $_POST['email'];
+    $pwd = $_POST['pwd'];
+
+    try{
+        require_once './../dbconnection.inc.php';
+        require_once './login-user-controller.inc.php';
+        require_once './login-user-model.inc.php';
+
+        $errors = check_valid_input($email, $pwd);
+
+        require_once './../session.inc.php';
+        if($errors) {
+            $_SESSION['errors_login'] = $errors;
+            header('Location: ./../../index.php');
+            die();
+        }
+
+        $is_correct = is_login_correct($pdo, $pwd, $email);
+
+        if(!$is_correct) {
+            $errors['invalid credentials'] = 'E-mail ou senha errados';
+            $_SESSION['errors_login'] = $errors;
+            header('Location: ./../../index.php');
+            die();
+        }
+        
+        header('Location: ./../../records.php');
+        $pdo = null;
+        $stmt = null;
+        die();
+    } catch (PDOException $e) {
+        die('Something went wrong : '.$e->getMessage());
+    }
+} else {
+    header('Location: ./../../index.php');
+    die();
+}
