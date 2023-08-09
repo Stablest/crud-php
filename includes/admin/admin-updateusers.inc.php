df035939c66bc1998b5990fcafa3eaaeb3f1dd81
Stablest <marlonlocci@hotmail.com>
2023-08-09 16:49:27 -0300

    misc: Removed unused code

--- includes/admin/admin-updateusers.inc.php
+++ includes/admin/admin-updateusers.inc.php
@@ -8,7 +8,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         $json = file_get_contents('php://input');
         $obj = json_decode($json);
-        // echo $obj->{'names'}->{'email'};
         $id = $obj->{'id'};
         $username = $obj->{'username'};
         $email = $obj->{'email'};
