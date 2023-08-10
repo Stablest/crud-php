88164e313347d73ea34dad19ae80e738e07b24c2
Stablest <marlonlocci@hotmail.com>
2023-08-10 07:15:06 -0300

    refactor: Welcome message is not a header anymore

--- public/header.php
+++
@@ -1,8 +0,0 @@
-<header>
-    <div class="fs-3">
-        <?php
-            if (isset($_SESSION['username']))
-                echo 'Welcome ' . '<span class="fw-semibold">' . $_SESSION['username'] . '</span>';
-        ?>
-    </div>
-</header>
---
+++ public/welcome-username.php
@@ -0,0 +1,8 @@
+<div>
+    <div class="fs-3">
+        <?php
+        if (isset($_SESSION['username']))
+            echo 'Welcome ' . '<span class="fw-semibold">' . $_SESSION['username'] . '</span>';
+        ?>
+    </div>
+</div>
