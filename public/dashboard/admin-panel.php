f687339f8057ac675c5c53b88c5438ba6a4f496d
Stablest <marlonlocci@hotmail.com>
2023-08-10 08:09:49 -0300

    refactor: Removed populate page script, Added HTML response from server side

--- public/dashboard/admin-panel.php
+++ public/dashboard/admin-panel.php
@@ -3,13 +3,6 @@
         <button class="" id="salvar">Salvar</button>
         <button class="" id="salvar">Deletar</button>
         <div id="container">
-
-        </div>
-    </div>
-</div>
-<script type='module'>
-    const populateDivs = (data) => {
-        let html = `
             <table class="bg-info border rounded-2">
                 <thead>
                     <tr>
@@ -19,105 +12,42 @@
                         <th class="px-3">permission</th>
                     </tr>
                 </thead>
-                <tbody>`
-        data.forEach((element, index) => {
-            html += `
-                <tr>
-                    <td class="px-3" data-value=${element.id}><input type="checkbox" name="check_${element.id}"></td>
-                    <td class="px-3" data-value=${element.id}><input type="text" hidden name="username_${element.id}" placeholder="Usuário"><span>${element.username}</span></td>
-                    <td class="px-3" data-value=${element.id}><input type="email" hidden name="email_${element.id}" placeholder="E-mail"><span>${element.email}</span></td>
-                    <td class="px-3" data-value=${element.id}><input type="text" hidden name="permission_${element.id}" placeholder="Permissão"><span>${element.permission}</span></td>
-                </tr>`
-        })
-        html += '</tbody></table>'
-        return html
-
-    }
-
-    const res = await fetch("./../../src/admin/admin-getusers.inc.php")
-    const data = await res.json();
-    const html = populateDivs(data);
-    $('#container').html(html)
-
-    //////////////////////
-
-    let changed_rows = []
-
-    const submit = $('#salvar').click(async function() {
-        const updatedDataList = []
-        changed_rows.forEach((row, index) => {
-            const spans = $(`td[data-value=${row.index}]>span`)
-            const inputs = $(`td[data-value=${row.index}]>input`)
-            const id = $(inputs[1]).parent().data('value')
-            const updatedData = {
-                id: id,
-                username: $(inputs[1]).prop('value').length === 0 ? $(spans[0]).text() : $(inputs[1]).prop('value'),
-                email: $(inputs[2]).prop('value').length === 0 ? $(spans[1]).text() : $(inputs[2]).prop('value'),
-                permission: $(inputs[3]).prop('value').length === 0 ? $(spans[2]).text() : $(inputs[3]).prop('value')
-            }
-            updatedDataList.push(updatedData)
-        })
-        try {
-            const promises = []
-            updatedDataList.forEach((updatedData) => {
-                promises.push($.ajax({
-                    url: './../../src/user/user.inc.php',
-                    type: 'PUT',
-                    data: JSON.stringify(updatedData)
-                }).promise())
-                // promises.push($.post('./../../src/user/user.inc.php', JSON.stringify(updatedData)).promise())
-            })
-            const result = await Promise.all(promises)
-        } catch (e) {
-            console.error('Something went wrong')
-        }
-    })
-
-    function create_new_row(name) {
-        let newName = {
-            username: null,
-            email: null,
-            permission: null,
-        }
-
-        switch (name) {
-            case 'username':
-                newName.username = name
-                break
-            case 'email':
-                newName.email = name
-                break
-            case 'permission':
-                newName.permission = name
-                break
-        }
-        return newName
-    }
-
-    const tds = $('td').click(function() {
-        const input = $(this).children('input')
-        const span = $(this).children('span');
-        if (input.attr('hidden') === 'hidden') {
-            input.attr('hidden', false)
-            span.attr('hidden', true)
-            const index = $(this).data('value')
-            const name = input.attr('name').split('_', 1)[0]
-            let isRepeated = false
-            if (changed_rows.length > 0) {
-                changed_rows.forEach((element) => {
-                    if (index === element.index) {
-                        element.names[name] = name
-                        isRepeated = true
+                <tbody>
+                    <?php
+                    try {
+                        require_once './../../src/dbconnection.inc.php';
+                        require_once './../../src/user/user-controller.inc.php';
+                        require_once './../../src/user/user-model.inc.php';
+                        require_once './../../src/session.inc.php';
+                        $users = get_all_users($pdo);
+                        $pdo = null;
+                        $stmt = null;
+                    } catch (PDOException $e) {
+                        die('Something went wrong : ' . $e->getMessage());
                     }
-                })
-                if (isRepeated)
-                    return
-            }
-            const newName = create_new_row(name)
-            changed_rows.push({
-                names: newName,
-                index: index,
-            })
-        }
-    })
-</script>
+                    foreach ($users as $user) { ?>
+                        <tr>
+                            <td class="px-3" data-value="<?php echo htmlspecialchars($user['id']) ?>">
+                                <input type="checkbox" name="check_<?php echo htmlspecialchars($user['id']) ?>" />
+                            </td>
+                            <td class="px-3" data-value="<?php echo htmlspecialchars($user['id']) ?>">
+                                <input type="text" hidden name="username_<?php echo htmlspecialchars($user['id']) ?>" placeholder="Usuário" />
+                                <span><?php echo htmlspecialchars($user['username']) ?></span>
+                            </td>
+                            <td class="px-3" data-value="<?php echo htmlspecialchars($user['id']) ?>">
+                                <input type="email" hidden name="email_<?php echo htmlspecialchars($user['id']) ?>" placeholder="E-mail" />
+                                <span><?php echo htmlspecialchars($user['email'])  ?></span>
+                            </td>
+                            <td class="px-3" data-value="<?php echo htmlspecialchars($user['id']) ?>">
+                                <input type="text" hidden name="permission_<?php echo htmlspecialchars($user['id']) ?>" placeholder="Permissão" />
+                                <span><?php echo htmlspecialchars($user['permission'])  ?></span>
+                            </td>
+                        </tr>
+                    <?php } ?>
+                </tbody>
+            </table>;
+
+        </div>
+    </div>
+</div>
+<script type="module" src='./../helpers/scripts/admin-panel-table-interact.js'></script>
